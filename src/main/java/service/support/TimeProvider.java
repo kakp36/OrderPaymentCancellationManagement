34e270579ceb31b2bb6d8f469ea95f7cc86fbf22
jia xiaotian <jiaxiaotian520@gmail.com>
2026-01-17 20:22:09 +0900

    時間を取得する機能を追加した。

--- src/main/java/service/support/TimeProvider.java
+++ src/main/java/service/support/TimeProvider.java
@@ -1,4 +1,10 @@
 package service.support;
+
+import java.time.LocalDateTime;
+
 //時間取得を1か所にまとめる
 public class TimeProvider {
+    public LocalDateTime now() {
+        return LocalDateTime.now();
+    }
 }
